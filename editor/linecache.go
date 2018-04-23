bd1fcfb4e28854bd30c1e1872aed2082da7d99c3
Sujit Joshi <joshi.sujit020386@gmail.com>
2018-04-23 06:16:26 -0400

    Fix the "update" case in linecache.go and remove the addLines method (#14)
    * Fixing linecache
    
    * linde12/kod/editor
    
    * Removing comments
    
    * Cleanup of Update Op in linecache

--- editor/linecache.go
+++ editor/linecache.go
@@ -25,25 +25,11 @@ func (lc *LineCache) addInvalid(newLines []*Line, newInvalidBefore *int, newInva
 	}
 }
 
-func (lc *LineCache) addLine(newLines []*Line, newInvalidBefore *int, newInvalidAfter *int, line *Line) []*Line {
-	if line != nil {
-		for i := 0; i < *newInvalidAfter; i++ {
-			newLines = append(newLines, nil)
-		}
-		*newInvalidAfter = 0
-		newLines = append(newLines, line)
-	} else {
-		lc.addInvalid(newLines, newInvalidBefore, newInvalidAfter, 1)
-	}
-	return newLines
-}
-
 func (lc *LineCache) ApplyUpdate(update *rpc.Update) {
 	// TODO: Make capacity = height of buffer
 	newLines := make([]*Line, 0, 10)
 	newInvalidBefore := 0
 	newInvalidAfter := 0
-	index := 0
 
 	for _, op := range update.Update.Ops {
 		switch op.Op {
@@ -103,14 +89,6 @@ func (lc *LineCache) ApplyUpdate(update *rpc.Update) {
 			for _, line := range op.Lines {
 				newline := NewLine(line.Text, line.Cursor, line.Styles)
 				newLines = append(newLines, newline)
-			}
-		case "update":
-			for _, line := range op.Lines {
-				lineToUpdate := lc.lines[index-lc.invalidBefore]
-				lineToUpdate.Cursors = line.Cursor
-				lineToUpdate.SetStyles(line.Styles)
-				newLines = lc.addLine(newLines, &newInvalidBefore, &newInvalidAfter, lineToUpdate)
-				index++
 			}
 		}
 	}
